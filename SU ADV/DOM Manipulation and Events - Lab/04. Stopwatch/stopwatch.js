07d21177df3bf6f6d60ecb6e95915d4a4f554e75
nkapzamalov <nkapzamalov@gmail.com>
2022-10-20 11:50:42 +0300

    Add Homework

--- SU ADV/DOM Manipulation and Events - Lab/04. Stopwatch/stopwatch.js
+++ SU ADV/DOM Manipulation and Events - Lab/04. Stopwatch/stopwatch.js
@@ -1,3 +1,30 @@
 function stopwatch() {
-    console.log('TODO:...');
+  let timeElement = document.getElementById("time");
+  let startButtonElement = document.getElementById("startBtn");
+  let stopButtonElement = document.getElementById("stopBtn");
+  let interval;
+  startButtonElement.addEventListener("click", function (e) {
+    timeElement.textContent = "00:55";
+    stopButtonElement.removeAttribute("disabled");
+    startButtonElement.setAttribute("disabled", true);
+    interval = setInterval(function () {
+      let timeArray = timeElement.textContent.split(":");
+      let minutes = Number(timeArray[0]);
+      let seconds = Number(timeArray[1]);
+      seconds++;
+      if (seconds > 59) {
+        minutes++;
+        seconds = 0;
+      }
+      timeElement.textContent = `${minutes
+        .toString()
+        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    }, 1000);
+  });
+
+  stopButtonElement.addEventListener("click", function (e) {
+    startButtonElement.removeAttribute("disabled");
+    stopButtonElement.setAttribute("disabled", true);
+    clearInterval(interval);
+  });
 }
--- SU ADV/DOM Manipulation and Events - Lab/07. Dynamic-Validation/app.js
+++ SU ADV/DOM Manipulation and Events - Lab/07. Dynamic-Validation/app.js
@@ -1,3 +1,18 @@
 function validate() {
-    console.log('TODO:...');
+  let inputElement = document.getElementById("email");
+
+  inputElement.addEventListener("change", function (e) {
+    console.log(e.target.value);
+
+    let input = e.target.value;
+    console.log(input);
+
+    let pattern = /[a-z]+@[a-z]+.[a-z]+/;
+
+    if (!pattern.test(input)) {
+      inputElement.classList.add("error");
+    } else {
+      inputElement.classList.remove("error");
+    }
+  });
 }
